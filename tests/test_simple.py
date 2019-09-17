4bd1d0f75f11f2f74af4e1e1282285d77561244d
Max Kowalski <maximalkowalski@gmail.com>
2019-09-18 01:15:33 +0300

    v 0.1

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -1,13 +1,23 @@
-# from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 
 def test_ya_simple():
-    assert 3 == 3
 
-    # driver = WebDriver(executable_path='C://selenium//chromedriver.exe')
-    #
-    # driver.get('https://yandex.ru')
-    #
-    # assert driver.title == 'Яндекс'
-    #
-    # driver.close()
+    chrome = webdriver.Remote(
+        command_executor='http://localhost:4444/wd/hub',
+        desired_capabilities=DesiredCapabilities.CHROME)
+    firefox = webdriver.Remote(
+        command_executor='http://localhost:4444/wd/hub',
+        desired_capabilities=DesiredCapabilities.FIREFOX)
+
+    chrome.get('https://yandex.ru')
+    assert chrome.title == 'Яндекс'
+    print(chrome.title)
+
+    firefox.get('https://yandex.ru')
+    assert firefox.title == 'Яндекс'
+    print(firefox.title)
+
+    chrome.quit()
+    firefox.quit()
