1cf56b97329e70f117128459fe1e36d88209a1a3
Max Kowalski <maximalkowalski@gmail.com>
2019-09-18 01:28:39 +0300

    v 0.2

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -5,10 +5,10 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 def test_ya_simple():
 
     chrome = webdriver.Remote(
-        command_executor='http://localhost:4444/wd/hub',
+        command_executor='http://172.17.0.2:4444/wd/hub',
         desired_capabilities=DesiredCapabilities.CHROME)
     firefox = webdriver.Remote(
-        command_executor='http://localhost:4444/wd/hub',
+        command_executor='http://172.17.0.2:4444/wd/hub',
         desired_capabilities=DesiredCapabilities.FIREFOX)
 
     chrome.get('https://yandex.ru')
