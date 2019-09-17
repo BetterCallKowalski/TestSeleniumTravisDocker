0a4b37db2181590faa8a07b561a3e5caee87d1eb
Max Kowalski <maximalkowalski@gmail.com>
2019-09-18 01:42:33 +0300

    v 0.4

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -4,20 +4,12 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 def test_ya_simple():
 
-    chrome = webdriver.Remote(
-        command_executor='http://172.17.0.2:4444/wd/hub',
-        desired_capabilities=DesiredCapabilities.CHROME)
     firefox = webdriver.Remote(
         command_executor='http://172.17.0.2:4444/wd/hub',
         desired_capabilities=DesiredCapabilities.FIREFOX)
-
-    chrome.get('https://yandex.ru')
-    assert chrome.title == 'Яндекс'
-    print(chrome.title)
 
     firefox.get('https://yandex.ru')
     assert firefox.title == 'Яндекс'
     print(firefox.title)
 
-    chrome.quit()
     firefox.quit()
